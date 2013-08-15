dd3444f5148638c038ed26b49f7ff78752c70694
Ninghang Hu <n.hu@uva.nl>
2013-08-15 17:12:48 +0200

    cccp_error takes two inference algorithm to compute the score

--- cccp_error.m
+++ cccp_error.m
@@ -1,37 +1,22 @@
 function cumError = cccp_error(params,trainData,model)
+% after learning, model holds new parameters model.w, recompute optimal
+% value to measure cumulative error over data
 
 cumError = 0;
 
 for i = 1 : length(params.labels)
-    
-    
+       
     X = params.patterns{i};
     Y = trainData.labels{i};
-    Z = inferLatentVariable(params, model, X, Y);
-    YZ = sub2indYZ(params,Y,Z);
-    
-    K = length(X) / params.DimX; % length of sequence 
-    factors = build_graphical_factors(X,params,model,[]);
-    
-    % add loss factors %% TODO
-    loss_factors = repmat(struct('var', [], 'card', [], 'val', []), K, 1);
-    for k = 1 : K
-        loss_factors(k) = compute_loss_factor(params, YZ, k);
-    end
-    
-    % combine all factors
-    all_factors = [factors;loss_factors];
+    YZ = sub2ind(params.szYZ, Y, ones(size(Y))); % Z does not matter when computing loss function
     
     % argmax_y delta(yi, y) + <psi(x,y), w>
-    Fi = RunInference(all_factors,'pd');
+    [~,Fi] = constraintCB(params, model, X, YZ);
     
-    
-    % build graphical model: factors that violate observed Y are set to zero
-    factors = build_graphical_factors(X,params,model,Y);
+    %%% ----------------------------------------- %%%
     
     % argmax_z <phi(x,y,z), w>
-    Gi = RunInference(factors,'pd');
-    
+    [~,Gi] = inferLatentVariable(params, model, X, Y);
     
     %%% error of data point %%% %% TODO
     cumError = cumError + abs(Fi-Gi);
