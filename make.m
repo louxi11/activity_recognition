f13cfb1a42104dc125b5213b75f3e5a4cae9a3c3
Ninghang Hu <n.hu@uva.nl>
2013-08-15 14:26:27 +0200

    updated make.m

--- make.m
+++ make.m
@@ -1,17 +1,26 @@
 clc
+
+fprintf('\n***************************************\n')
+
 cd svm-struct-matlab-1.2/
 if system('make')
   error('compile structued svm failed, check the error messages\n')
 end
 cd ..
 
+fprintf('\n***************************************\n')
+
 cd inference/libdai/
 if system('make')
   error('compile libdai failed, check the error messages\n')
 end
 
-disp('cp build/doinference ../doinference-linux')
+fprintf('\n***************************************\n');
+
+fprintf('\ncp build/doinference ../doinference-linux\n')
 system('cp build/doinference ../doinference-linux');
 cd ../../
 
-disp('all done.')
+fprintf('\n***************************************\n');
+
+fprintf('all done.')
