d9df5b5214ba96fc5ab4c5b50a3d5327288604db
Ninghang Hu <n.hu@uva.nl>
2013-08-17 11:34:01 +0200

    updated texts

--- inference/RunInference.m
+++ inference/RunInference.m
@@ -6,11 +6,13 @@ for i = 1 : length(factors)
 
   fg{i}.Member = factors(i).var;
   if length(factors(i).var) == 1
-    sz = [factors(i).card,1];
+    % if only one var, column vector
+    sz = [factors(i).card,1]; 
   else
+    % if multiple vars, size(P) = cardinality
     sz = factors(i).card;
   end
-  fg{i}.P = reshape(factors(i).val, sz); %% TODO
+  fg{i}.P = reshape(factors(i).val, sz); 
   
 %   % TODO better performance on precision?
 %   fg{i}.P = reshape(roundn(factors(i).val,-7), sz);
