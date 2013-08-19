7640ffd4768af669259f039b3abfb599090603d1
Ninghang Hu <n.hu@uva.nl>
2013-08-19 09:53:15 +0200

    update text

--- inference/RunInference.m
+++ inference/RunInference.m
@@ -6,10 +6,10 @@ for i = 1 : length(factors)
 
   fg{i}.Member = factors(i).var;
   if length(factors(i).var) == 1
-    % if only one var, column vector
+    % if only one var, fg.P is a column vector
     sz = [factors(i).card,1]; 
   else
-    % if multiple vars, size(P) = cardinality
+    % if multiple vars, size(fg.P) = variable cardinality
     sz = factors(i).card;
   end
   fg{i}.P = reshape(factors(i).val, sz); 
