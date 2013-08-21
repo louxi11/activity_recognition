28cffa628c89e0a30548db5fff64a9bec85e9853
Ninghang Hu <n.hu@uva.nl>
2013-08-21 11:32:21 +0200

    added temporal parse and train_sid options for loading CAD120 dataset

--- test_data/load_CAD120.m
+++ test_data/load_CAD120.m
@@ -1,6 +1,16 @@
-function [trainData,testData] = load_CAD120(parse)
+function [trainData,testData] = load_CAD120(parse,tfeat,train_sid)
 
-if parse
+parse_opts = {'parse_on','parse_off'};
+tfeat_opts = {'tfeat_on','tfeat_off'};
+
+% validate input arguments
+if ~sum(strcmp(parse,parse_opts))
+    error('load_CAD120:invalid input argument:','parse should be either "parse_on" or "parse_off"');
+elseif ~sum(strcmp(tfeat,tfeat_opts))
+    error('load_CAD120:invalid input argument:','tfeat should be either "tfeat_on" or "tfeat_off"');
+end
+
+if strcmp(parse,'parse_on')
     cd test_data/CAD120/
     parse_CAD120
     cd ../../
@@ -8,13 +18,47 @@ end
 
 load test_data/CAD120/CAD120.mat
 
-trainidx = 1:110; % TODO change back
-testidx = 111:125;
 
-trainData.patterns = X(trainidx); 
+% replace data with tfeatures if the switch is turned on
+if strcmp(tfeat,'tfeat_on') 
+    X = Xt;
+    Y = Yt;
+    vid_id = int32(str2double(vid_id_t));
+    FEATURE_LENGTH = TEMPORAL_FEATURE_LENGTH;
+else
+    vid_id = int32(str2double(vid_id));
+end
+
+
+% split training and test set based on subject id
+if isempty(train_sid)
+    trainidx = 1:110; % default
+    testidx = 111:125;
+else
+    all_sid = 1 : 4;
+    test_sid = all_sid(~ismember(all_sid,train_sid));
+    
+    cd test_data/CAD120/
+    subjects = parse_subject;
+    cd ../../
+    
+    train_videos = [subjects{train_sid}];
+    train_videos = train_videos(:);
+    test_videos = [subjects{test_sid}];
+    test_videos = test_videos(:);
+    
+    [~,trainidx,~] = intersect(vid_id,train_videos);
+    [~,testidx,~] = intersect(vid_id,test_videos);
+    
+end
+
+
+% load final training set and test set
+trainData.patterns = X(trainidx);
 trainData.labels = Y(trainidx);
-
+trainData.DimX = FEATURE_LENGTH;
 testData.patterns = X(testidx);
 testData.labels = Y(testidx);
+testData.DimX = FEATURE_LENGTH;
 
 end
