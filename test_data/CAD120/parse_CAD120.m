2c9c01364bc8e6e5fb5606636ce30b2513c8a12e
Ninghang Hu <n.hu@uva.nl>
2013-08-21 13:04:56 +0200

    update

--- test_data/CAD120/parse_CAD120.m
+++ test_data/CAD120/parse_CAD120.m
@@ -1,46 +1,46 @@
-% % LOAD CAD120 DATASET TO MATLAB
-% 
+% LOAD CAD120 DATASET TO MATLAB
+
 % clear all
-% 
-% vidfiles = dir('segments_svm_format/*.txt'); % activity segments
-% 
-% video = cell(length(vidfiles),1);
-% 
-% num_objects = 5; % TODO: USE CLASS OF OBJECTS INSTEAD
-% 
-% for v = 1 : length(vidfiles)
-%     
-%     vidfile = fullfile('segments_svm_format/',vidfiles(v).name);
-%     
-%     % parse video file into segments
-%     [sfile,tfile] = parse_video(vidfile);
-%     
-%     data = repmat(struct('features',[],'N1',[],'E1',[],'E2',[],...
-%         'K1',[],'K2',[],'FN',[],'sub_activity',[],...
-%         'object_id',[],'skel_id',[],'affordance',[],...
-%         'filename',[]), length(sfile), 1);
-%     
-%     % parse features in one segment
-%     for s = 1 : length(sfile)  
-%         data(s) = parse_segments(sfile{s},num_objects);
-%     end
-%     
-%     % parse features between consecutive segments
-%     for s = 1 : length(tfile)
-%         data = parse_temporal_segments(data, tfile{s}, num_objects);
-%     end
-% 
-%     video{v} = data;
-%     
-% end
-% 
-% sz = whos('video');
-% 
-% sz = sz.bytes/1024/1024;
-% 
-% fprintf('\nCAD120 loaded successfully. Total size = %.2fMB\n',sz);
-% 
-% save('CAD120video.mat','video')
+
+vidfiles = dir('segments_svm_format/*.txt'); % activity segments
+
+video = cell(length(vidfiles),1);
+
+num_objects = 5; % TODO: USE CLASS OF OBJECTS INSTEAD
+
+for v = 1 : length(vidfiles)
+    
+    vidfile = fullfile('segments_svm_format/',vidfiles(v).name);
+    
+    % parse video file into segments
+    [sfile,tfile] = parse_video(vidfile);
+    
+    data = repmat(struct('features',[],'N1',[],'E1',[],'E2',[],...
+        'K1',[],'K2',[],'FN',[],'sub_activity',[],...
+        'object_id',[],'skel_id',[],'affordance',[],...
+        'filename',[]), length(sfile), 1);
+    
+    % parse features in one segment
+    for s = 1 : length(sfile)  
+        data(s) = parse_segments(sfile{s},num_objects);
+    end
+    
+    % parse features between consecutive segments
+    for s = 1 : length(tfile)
+        data = parse_temporal_segments(data, tfile{s}, num_objects);
+    end
+
+    video{v} = data;
+    
+end
+
+sz = whos('video');
+
+sz = sz.bytes/1024/1024;
+
+fprintf('\nCAD120 loaded successfully. Total size = %.2fMB\n',sz);
+
+save('CAD120video.mat','video')
 
 
 %% CONVERT TO FORMAT OF STRUCTUED-SVM
