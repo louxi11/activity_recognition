c197e74f28b4d8ab70d6202cfd2f08df44e42893
Ninghang Hu <n.hu@uva.nl>
2013-08-28 20:02:09 +0200

    changed new one-of-N encoding for affordance

--- test_data/CAD120/encodeAffordance.m
+++ test_data/CAD120/encodeAffordance.m
@@ -1,10 +1,16 @@
 function afford = encodeAffordance(A,numAfford)
 % encode object affordance
 
-sz = [numAfford + 1,length(A)];
-afford = zeros(prod(sz),1);
-A(isnan(A)) = numAfford + 1;
-idx = sub2ind(sz,A,1:length(A));
-afford(idx) = 1;
+sz = [numAfford,length(A)];
+afford = zeros(sz);
+for i = 1 : length(A)
+  a = A(i);
+  if isnan(a)
+    continue
+  else 
+    afford(a,i) = 1;
+  end
+end
+afford = afford(:);
 
 end
