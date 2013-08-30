68c037edeb59082af6892529975665d4415e5aac
Ninghang Hu <n.hu@uva.nl>
2013-08-30 11:47:57 +0200

    updated readme and template

--- evaluation_template.m
+++ evaluation_template.m
@@ -15,7 +15,7 @@ addpath test_data/
 save_on = 1;
 
 %%% parameters %%%
-numStateZ = 2;
+numStateZ = 4;
 C = 0.3; % normalization constant
 E = 0.25; % epsilon
 W = 3; % optimization strategy
@@ -23,18 +23,23 @@ tfeat = 'tfeat_on';
 thres = inf; % threshold to stop iteration TODO
 initStrategy = 'affordance';
 
-eval_set = 1;
+eval_set = 1:3;
+iter = 1;
 
 %%% allocate buffer %%%
 trainRate = nan(4,length(eval_set));
 testRate = nan(4,length(eval_set));
+prec = nan(4,length(eval_set));
+recall = nan(4,length(eval_set));
+fscore = nan(4,length(eval_set));
+confmat = cell(4,length(eval_set));
 
+% 4 fold cross-validation
 combos = combntns(1:4,3);
 
-for iter = 1 : length(eval_set)
+for c = 1 : length(eval_set)
   
-  GT = [];
-  PRED = [];
+  iter = eval_set(c);
   
   %%% learning %%%
   for i = 1 : size(combos,1)
@@ -45,10 +50,10 @@ for iter = 1 : length(eval_set)
     test_sid = all_sid(~ismember(all_sid,train_sid));
     
     filebase = sprintf('Z%d_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s_iter%d',numStateZ,C,E,W,tfeat,thres,initStrategy,iter);
-    if save_on
+%     if save_on
       logfile = sprintf([filebase,'_Test%d'],test_sid);
-      make_log(logfile); % LOG file and SAVE MODEL
-    end
+%       make_log(logfile); % LOG file and SAVE MODEL
+%     end
     
     % load structured svm options
     learning_option = sprintf('-c %.2f -e %.2f -w %d',C,E,W); % ssvm learning parameters
@@ -56,14 +61,16 @@ for iter = 1 : length(eval_set)
     % split training and test data
     [trainData,testData] = load_CAD120('parse_off',tfeat,train_sid);
     
-    % learning
-    [model,params] = learning_CAD120(trainData,numStateZ,learning_option,thres,initStrategy);
+%     % learning
+%     [model,params] = learning_CAD120(trainData,numStateZ,learning_option,thres,initStrategy);
     
-    % save model to file
-    if save_on
-      save(['model_',logfile,'.mat'],'model','params','trainData','testData')
-    end
+%     % save model to file
+%     if save_on
+%       save(['model_',logfile,'.mat'],'model','params','trainData','testData')
+%     end
+load(['model_',logfile,'.mat'],'model','params','trainData','testData')
     
+
     %%% classification %%%
     
     CNT = 0;
@@ -75,10 +82,12 @@ for iter = 1 : length(eval_set)
       D = D + sum( int32(data.labels{j}) == int32(yhat));
       CNT = CNT + length(data.labels{j});
     end
-    trainRate(i,iter) = D/CNT;
+    trainRate(i,c) = D/CNT;
     
     CNT = 0;
     D = 0;
+    GT = [];
+    PRED = [];
     data = testData;
     for j = 1 : length(data.patterns)
       X_test = data.patterns{j};
@@ -88,13 +97,24 @@ for iter = 1 : length(eval_set)
       D = D + sum( int32(data.labels{j}) == int32(yhat));
       CNT = CNT + length(data.labels{j});
     end
-    testRate(i,iter) = D/CNT;
+    testRate(i,c) = D/CNT;
+    
+    
+    [confmat0, prec0, recall0, fscore0] = prec_recall(GT,PRED);
+    prec(i,c) = mean(prec0);
+    recall(i,c) = mean(recall0);
+    fscore(i,c) = mean(fscore0);
+    confmat{i,c} = confmat;
     
     fprintf('******************************\n')
     fprintf('Training set: %d, %d, %d\n',train_sid(1),train_sid(2),train_sid(3));
-    fprintf('Training rate: %.4f\n\n',trainRate(i,iter));
+    fprintf('Training rate: %.4f\n\n',trainRate(i,c));
+    
     fprintf('Test set: %d\n',test_sid);
-    fprintf('Test rate: %.4f\n\n',testRate(i,iter));
+    fprintf('Test rate: %.4f\n',testRate(i,c));
+    fprintf('Test precision: %.4f\n',prec(i,c));
+    fprintf('Test recall: %.4f\n',recall(i,c));
+    fprintf('Test Fscore: %.4f\n',fscore(i,c));
     fprintf('******************************\n\n')
     
     diary off
@@ -102,26 +122,18 @@ for iter = 1 : length(eval_set)
   end
   
   
-  [confmat, prec, recall, fscore] = prec_recall(GT,PRED);
   
-  results.meanTrain = mean(trainRate);
-  results.stdTrain = std(trainRate);
-  results.meanTest = mean(testRate);
-  results.stdTest = std(testRate);
+  results.meanTrain = mean(trainRate(:));
+  results.stdTrain = std(trainRate(:));
+  results.meanTest = mean(testRate(:));
+  results.stdTest = std(testRate(:));
   
-  results.meanPrec = mean(prec);
-  results.stdPrec = std(prec);
-  results.meanRecall = mean(recall);
-  results.stdRecall = std(recall);
-  results.meanFscore = mean(fscore);
-  results.stdFscore = std(fscore);
-  
-  fprintf('******************************\n')
-  fprintf('Mean Test rate: %.4f\n',mean(testRate));
-  fprintf('Mean Test precision: %.4f\n',mean(prec));
-  fprintf('Mean Test recall: %.4f\n',mean(recall));
-  fprintf('Mean Test Fscore: %.4f\n',mean(fscore));
-  fprintf('******************************\n\n')
+  results.meanPrec = mean(prec(:));
+  results.stdPrec = std(prec(:));
+  results.meanRecall = mean(recall(:));
+  results.stdRecall = std(recall(:));
+  results.meanFscore = mean(fscore(:));
+  results.stdFscore = std(fscore(:));
   
   if save_on
     save([filebase,'.mat'],...
