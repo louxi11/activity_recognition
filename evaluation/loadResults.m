03ce9c195b0d134d59262a08a26268f999888745
Ninghang Hu <n.hu@uva.nl>
2013-09-26 07:34:33 +0200

    update

--- evaluation/loadResults.m
+++ evaluation/loadResults.m
@@ -1,4 +1,4 @@
-numStateZ = 1;
+numStateZ = 3;
 
 C = 0.3;
 E = 0.25;
@@ -8,21 +8,21 @@ thres = 1;
 initStrategy = 'clustering';
 iter = 3;
 
-disp('Z1')
-Z1 = [];
-for corruptPercentage = 0.2:0.1:0.8
+disp('Z3')
+Z3 = [];
+for corruptPercentage = 0:0.1:0.9
   filebase = sprintf('Z%d_cp_%02f_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s_iter%d.mat',numStateZ,corruptPercentage,C,E,W,tfeat,thres,initStrategy,iter);
   load(filebase)
   fscore = 2*prec.*recall./(prec+recall);
   m = nanmean(fscore(:));
   s = nanstd(fscore(:));
   a = results.meanTest;
-  Z1 = [Z1;[m,s,a]];
+  Z3 = [Z3;[m,s,a]];
 end
 
-disp('Z2')
-numStateZ = 2;
-Z2 = [];
+disp('Z4')
+numStateZ = 4;
+Z4 = [];
 for corruptPercentage = 0.2:0.1:0.8
   filebase = sprintf('Z%d_cp_%02f_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s_iter%d.mat',numStateZ,corruptPercentage,C,E,W,tfeat,thres,initStrategy,iter);
   load(filebase)
@@ -30,12 +30,12 @@ for corruptPercentage = 0.2:0.1:0.8
   m = nanmean(fscore(:));
   s = nanstd(fscore(:));
   a = results.meanTest;
-  Z2 = [Z2;[m,s,a]];
+  Z4 = [Z4;[m,s,a]];
 end
 
 hold off
 % errorbar(Z1(:,1),Z1(:,2));
-plot(0.2:0.1:0.8,Z1(:,1)*100)
+plot(0:0.1:0.9,Z1(:,1)*100)
 hold on
 % errorbar(Z2(:,1),Z2(:,2),'r')
 plot(0.2:0.1:0.8,Z2(:,1)*100,'r')
