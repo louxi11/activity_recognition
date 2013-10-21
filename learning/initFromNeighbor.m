109201257c010b172a4ab02d60add7bf1ea27f42
Ninghang Hu <n.hu@uva.nl>
2013-10-21 16:05:20 +0200

    added initFromNeighbor

---
+++ learning/initFromNeighbor.m
@@ -0,0 +1,36 @@
+function label = initFromNeighbor(label,params)
+
+% params.numStateY = 10;
+
+% label = (1:6)';
+% label([1,2,4,6]) = nan;
+
+% label
+
+l = label;
+
+nan_mask = isnan(l);
+idx = find(nan_mask);
+nan_mask = [false;nan_mask;false];
+l = [nan;l;nan];
+lidx = [nan_mask(2:end);false];
+ridx = [false;nan_mask(1:end-1)];
+a = [l(lidx),l(ridx)];
+
+s = sum(isnan(a),2);
+
+% Neigther neighbor is valid -> random label
+label(idx(s == 2)) = randsample(params.numStateY,sum(s==2),true);
+
+% one neighbor is valid -> init with valid neighbor
+b = a(s == 1,:);
+b=b';
+label(idx(s == 1)) = b(~isnan(b));
+
+% both neighbors are valid -> init with either of them
+c = a(s == 0,:);
+if ~isempty(c)
+  label(idx(s == 0)) = datasample(c,1);
+end
+
+end
