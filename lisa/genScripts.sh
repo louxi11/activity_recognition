f3e67faffc995fbcdf7fcf4d7b3a822d353e8662
Ninghang Hu <n.hu@uva.nl>
2013-10-24 19:32:09 +0200

    modified genScripts.sh for numStateZ

--- lisa/genScripts.sh
+++ lisa/genScripts.sh
@@ -10,12 +10,13 @@ for E in 0.2 0.4 0.6 0.8 1.0 1.3 1.6 2.0 2.5 3.0
 do
     for C in 0.2 0.4 0.6 0.8 1.0 1.3 1.6 2.0 2.5 3.0
     do
-
-        echo "#PBS -lnodes=1:ppn=1" > ./job_$E\_$C
-        echo "#PBS -lwalltime=100:00:00" >> ./job_$E\_$C
-        echo "module load mcr" >> ./job_$E\_$C
-        echo "cd ~/workspace/activity_recognition" >> ./job_$E\_$C
-        echo "$1 $C $E $thres $baseFile $corruptPercentage" >> ./job_$E\_$C
-
+        for numStateZ in {1..4}
+        do
+            echo "#PBS -lnodes=1:ppn=1" > ./job_$E\_$C\_$numStateZ
+            echo "#PBS -lwalltime=100:00:00" >> ./job_$E\_$C\_$numStateZ
+            echo "module load mcr" >> ./job_$E\_$C\_$numStateZ
+            echo "cd ~/workspace/activity_recognition" >> ./job_$E\_$C\_$numStateZ
+            echo "$1 $numStateZ $C $E $thres $baseFile $corruptPercentage" >> ./job_$E\_$C\_$numStateZ
+        done
     done
 done
