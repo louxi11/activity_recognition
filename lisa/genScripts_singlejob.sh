f66f5c41dd9b97f9dce084d76ada02c23b95946b
Ninghang Hu <n.hu@uva.nl>
2013-10-25 14:24:01 +0200

    update

--- lisa/genScripts_singlejob.sh
+++ lisa/genScripts_singlejob.sh
@@ -11,9 +11,9 @@ E=0.25
 C=0.3
 for numStateZ in {1..4}
 do
-    echo "#PBS -lnodes=1:ppn=1" > ./job_$E\_$C\_$numStateZ
-    echo "#PBS -lwalltime=100:00:00" >> ./job_$E\_$C\_$numStateZ
-    echo "module load mcr" >> ./job_$E\_$C\_$numStateZ
-    echo "cd ~/workspace/activity_recognition" >> ./job_$E\_$C\_$numStateZ
-    echo "./semi_supervised_template $numStateZ $C $E $thres $baseFile $corruptPercentage $isparallel" >> ./job_$E\_$C\_$numStateZ\_$isparallel
+    echo "#PBS -lnodes=1:ppn=1" > ./job_E$E\_C$C\_Z$numStateZ
+    echo "#PBS -lwalltime=100:00:00" >> ./job_E$E\_C$C\_Z$numStateZ
+    echo "module load mcr" >> ./job_E$E\_C$C\_Z$numStateZ
+    echo "cd ~/workspace/activity_recognition" >> ./job_E$E\_C$C\_Z$numStateZ
+    echo "./semi_supervised_template $numStateZ $C $E $thres $baseFile $corruptPercentage $isparallel" >> ./job_E$E\_C$C\_Z$numStateZ
 done
