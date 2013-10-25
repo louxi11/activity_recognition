c48d6e7c2d77b1c39eaf86da40370bc794809dfa
Ninghang Hu <n.hu@uva.nl>
2013-10-25 14:09:12 +0200

    added isparallel option in genscript

--- lisa/genScripts.sh
+++ lisa/genScripts.sh
@@ -1,6 +1,7 @@
 thres=7
 corruptPercentage=inf
 baseFile=m2_500
+isparallel=true
 
 mkdir -p jobs
 cd jobs
@@ -16,7 +17,7 @@ do
             echo "#PBS -lwalltime=100:00:00" >> ./job_$E\_$C\_$numStateZ
             echo "module load mcr" >> ./job_$E\_$C\_$numStateZ
             echo "cd ~/workspace/activity_recognition" >> ./job_$E\_$C\_$numStateZ
-            echo "./semi_supervised_template $numStateZ $C $E $thres $baseFile $corruptPercentage" >> ./job_$E\_$C\_$numStateZ
+            echo "./semi_supervised_template $numStateZ $C $E $thres $baseFile $corruptPercentage $isparallel" >> ./job_$E\_$C\_$numStateZ\_$isparallel
         done
     done
 done
