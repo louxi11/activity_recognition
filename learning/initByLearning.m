67f5ff42691e9c63dc829e9d7b69117526273410
Ninghang Hu <n.hu@uva.nl>
2013-11-26 11:51:05 +0100

    initByLearning accepts Z>1

--- learning/initByLearning.m
+++ learning/initByLearning.m
@@ -5,9 +5,10 @@ assert(~isempty(model))
 labels = cell(trainData.patterns);
 for k = 1 : length(trainData.patterns)
   X = trainData.patterns{k};
-  Y = trainData.labels{k};
-  [~,~,Yhat] = inferLatentVariable(params,model,X,Y);
-  labels{k} = Yhat;
+  Y = trainData.labels{k};  
+  [Zhat,~,Yhat] = inferLatentVariable(params,model,X,Y);
+  YZhat = sub2indYZ(params,Yhat,Zhat);
+  labels{k} = YZhat; % TODO
 end
 
 end
