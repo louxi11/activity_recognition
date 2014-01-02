def2e3eb7d563555f9b98e1b209f34a5a928fec3
Ninghang Hu <n.hu@uva.nl>
2014-01-02 16:18:20 +0100

    added isave

---
+++ evaluation/isave.m
@@ -0,0 +1,5 @@
+function isave(file,model,params,trainData,testData)
+
+save(file,'model','params','trainData','testData')
+
+end
--- test_sample.m
+++ test_sample.m
@@ -1,15 +1,15 @@
 % test sample
 
 %%% parameters %%%
-numStateZ = '1';
+numStateZ = '2';
 C = '0.3'; % normalization constant
 E = '0.4'; % epsilon
 thres = '1'; % threshold to stop iteration TODO
-baseFile = 'groundtruth';
+baseFile = 'uniform_20_0';
 
 % turn on parrallel computing
 options = 'corrupt';
-flipProp = '0';
-numCores = '1'; % number of slaves
+flipProp = '0.1';
+numCores = '4'; % number of slaves
 
 semi_supervised_template(numStateZ,C,E,thres,baseFile,options,flipProp,numCores);
