b54f602f379e20f4f29bedbf8cbafc0d8549d901
Ninghang Hu <n.hu@uva.nl>
2014-02-24 10:00:16 +0100

    updated multi-class svm for returning confusion matrix

--- evaluation/evaluate_multi_class.m
+++ evaluation/evaluate_multi_class.m
@@ -1,80 +1,98 @@
 % test sample
 
-%%% parameters %%%
+set_global_path
+
+%%% sub-level parameters %%%
 numStateZ = 2;          % number of latent states
 C = 0.3;                % normalization constant - SVM parameter
 E = 0.4;                % epsilon - SVM parameter
-
-% BaseFolder = 'results_semi_supervised';
 BaseFolder = '';
-
-baseFiles = {'groundtruth','m1_100','m1_500','m1_1000'};
-
-%%%
+numFolds = 4;
+tfeat = 'tfeat_on';
 options = 'flip';      % available in next version, DO NOT CHANGE
 flipProb = 0;            % number of slaves in parrallel computing
 initStrategy = 'learning';
-tfeat = 'tfeat_on';
 W = 3;
 thres = 1;
+baseFiles = {'groundtruth','m1_100','m1_500','m1_1000','m2_100','m2_500','m2_1000'};
 
-% for b = 1 : length(baseFiles)
-%     baseFile = baseFiles{b};
-%     activity_recognition_demo(numStateZ,C,E,thres,baseFile,options, ...
-%                               flipProp,numCores);
-% end
-
-
-%
-maxZ = 20;
-numiter = 3;
-
-results = [];
+%%
+%%%
+numStateZmulti = 4;
 
 f = 1;
 baseFile = baseFiles{f};
-for numStateZmulti = 1 : maxZ
+results.confMAT = zeros(10,10);
+for testid = 1 : 4
+  iter = 1;
+  % load data
+  combos = combntns(1:numFolds,3);
+  train_sid = combos(testid,:);
+  all_sid = 1 : 4;
+  test_sid = all_sid(~ismember(all_sid,train_sid));
+  baseFolder = fullfile(pwd,'CAD120','segmentation_lists');
+  path = fullfile(baseFolder,baseFile);
+  [trainData,testData] = load_CAD120(tfeat,train_sid,path);
   
-  testRateUpper = nan(4,numiter);
-  prec = nan(4,numiter);
-  recall = nan(4,numiter);
-  fscore = nan(4,numiter);
+  % load model
+  dirResults = sprintf('opt_%s_Prob_%.2f_%s_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s',...
+    options,flipProb,baseFile,C,E,W,tfeat,thres,initStrategy);
+  filebase = sprintf('%s_Z%d_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s_iter%d',...
+    baseFile,numStateZ,C,E,W,tfeat,thres,initStrategy,iter);
+  file = fullfile(BaseFolder,dirResults,['model_',filebase,'_Test',num2str(test_sid),'.mat']);
+  load(file)
   
-  for testid = 1 : 4
-    
-    for iter = 1 : 3
-      % load data
-      numFolds = 4;
-      combos = combntns(1:numFolds,3);
-      train_sid = combos(testid,:);
-      all_sid = 1 : 4;
-      test_sid = all_sid(~ismember(all_sid,train_sid));
-      baseFolder = fullfile(pwd,'CAD120','segmentation_lists');
-      path = fullfile(baseFolder,baseFile);
-      [trainData,testData] = load_CAD120(tfeat,train_sid,path);
-      
-      % load model
-      dirResults = sprintf('opt_%s_Prob_%.2f_%s_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s',...
-        options,flipProb,baseFile,C,E,W,tfeat,thres,initStrategy);
-      filebase = sprintf('%s_Z%d_C%.2f_E%.2f_W%d_%s_Thre%.1f_%s_iter%d',...
-        baseFile,numStateZ,C,E,W,tfeat,thres,initStrategy,iter);
-      file = fullfile(BaseFolder,dirResults,['model_',filebase,'_Test',num2str(test_sid),'.mat']);
-      load(file)
-      
-      [~,testRateUpper(testid,iter),prec(testid,iter), recall(testid,iter), fscore(testid,iter)] = multi_class_svm(model,params,trainData,testData,numStateZmulti);
-      
-    end
-  end
-  
-  results.accMean(numStateZmulti) = mean2(testRateUpper);
-  results.accStd(numStateZmulti) = std2(testRateUpper);
-  results.precMean(numStateZmulti) = mean2(prec);
-  results.precStd(numStateZmulti) = std2(prec);
-  results.recallMean(numStateZmulti) = mean2(recall);
-  results.recallStd(numStateZmulti) = std2(recall);
-  results.fscoreMean(numStateZmulti) = mean2(fscore);
-  results.fscoreStd(numStateZmulti) = std2(fscore);
+  [trainRateUpper,testRateUpper,prec, recall, fscore,confMAT] = multi_class_svm(model,params,trainData,testData,numStateZmulti);
+  results.trainRateUpper(testid) = trainRateUpper;
+  results.testRateUpper(testid) = testRateUpper;
+  results.prec(testid) = prec;
+  results.recall(testid) = recall;
+  results.fscore(testid) = fscore;
+  results.confMAT = results.confMAT + confMAT;
 end
+
+% saveFile = ['results_multi_class_unigram_',baseFile];
+% save(saveFile,'results')
+
+%% Directly Evaluate High-level activities
+
+set_global_path
+f = 1;
+results.confMAT = zeros(10,10);
+for testid = 1 : 4
+  ZZ = 1;
+  % load data
+  baseFile = baseFiles{f};
+  combos = combntns(1:numFolds,3);
+  train_sid = combos(testid,:);
+  all_sid = 1 : 4;
+  test_sid = all_sid(~ismember(all_sid,train_sid));
+  baseFolder = fullfile(pwd,'CAD120','segmentation_lists');
+  path = fullfile(baseFolder,baseFile);
+  [trainData,testData] = load_CAD120(tfeat,train_sid,path);
+  
+  [trainRateUpper,testRateUpper, prec, recall, fscore,confMAT] = multi_class_svm_high_level(trainData,testData,ZZ);
+  results.trainRateUpper(testid) = trainRateUpper;
+  results.testRateUpper(testid) = testRateUpper;
+  results.prec(testid) = prec;
+  results.recall(testid) = recall;
+  results.fscore(testid) = fscore;
+  results.confMAT = results.confMAT + confMAT;
+end
+saveFile = ['results_multi_class_direct_',baseFile];
+save(saveFile,'results')
+%%
+%   end
+  
+%   results.accMean(numStateZmulti) = mean2(testRateUpper);
+%   results.accStd(numStateZmulti) = std2(testRateUpper);
+%   results.precMean(numStateZmulti) = mean2(prec);
+%   results.precStd(numStateZmulti) = std2(prec);
+%   results.recallMean(numStateZmulti) = mean2(recall);
+%   results.recallStd(numStateZmulti) = std2(recall);
+%   results.fscoreMean(numStateZmulti) = mean2(fscore);
+%   results.fscoreStd(numStateZmulti) = std2(fscore);
+% end
 
 saveFile = ['results_multi_class_unigram_',baseFile];
 save(saveFile,'results')
@@ -169,7 +187,7 @@ save(saveFile,'results')
 set_global_path
 
 %%% parameters %%%
-numStateZ = 1;          % number of latent states
+numStateZ = 2;          % number of latent states
 C = 0.3;                % normalization constant - SVM parameter
 E = 0.4;                % epsilon - SVM parameter
 
@@ -204,7 +222,7 @@ baseFile = baseFiles{f};
 eval_set = -6 : 0.5 : 3;
 
 for e = 1 : 19
-  for c = 1 : 19
+  for c = 20 : length(eval_set)
     EE = 2^eval_set(e);
     CC = 2^eval_set(c);
     testRateUpper = nan(4,1);
--- multi_class_svm.m
+++ multi_class_svm.m
@@ -1,4 +1,4 @@
-function [trainAccuracy,testAccuracy, prec, recall, fscore] = multi_class_svm(model,params,trainData,testData,numStateZ)
+function [trainAccuracy,testAccuracy, prec, recall, fscore, confMAT] = multi_class_svm(model,params,trainData,testData,numStateZ)
 % Multi-class SVM: sub-level activity as X, high-level activity as Y
 
 % compute input X and output Y for both training
@@ -10,12 +10,15 @@ end
 trainDataUpper.patterns = Xtrain;
 trainDataUpper.labels = num2cell(trainData.HighLabels);
 trainDataUpper.DimX = length(Xtrain{1});
-
+YYY = [];
+YYYY=[];
 % compute input X and output Y for both training
 Xtest = cell(size(testData.HighLabels)); % predicted labels
 for j = 1 : length(testData.patterns)
   x = testData.patterns{j};
   yhat = ssvm_classify(params, model, x);
+  YYY = [YYY;yhat];
+  YYYY = [YYYY;testData.labels{j}];
   Xtest{j} = [histc(yhat,1:params.numStateY);testData.occlusion_features(j,:)'];
 end
 testDataUpper.patterns = Xtest;
@@ -25,13 +28,16 @@ testDataUpper.labels = num2cell(testData.HighLabels);
 % numStateZ = 1;
 thres = 1;
 initStrategy = 'learning';
-C = 15;
-E = 0.1;
+% C = 15; % groundtruth high-level
+% E = 0.1;
+C = 10;
+E = 0.01;
+
 W = 3;
 hasPartialLabel = 0;
 hasLatent = 0;
 model = [];
-learning_option = sprintf('-c %.2f -e %.2f -w %d',C,E,W); % ssvm learning parameters
+learning_option = sprintf('-c %.6f -e %.4f -w %d',C,E,W); % ssvm learning parameters
 
 %
 [model_upper,params_upper] = learning_CAD120(trainDataUpper,numStateZ,learning_option,thres,initStrategy,C,model,hasPartialLabel,hasLatent);
@@ -39,7 +45,7 @@ learning_option = sprintf('-c %.2f -e %.2f -w %d',C,E,W); % ssvm learning parame
 
 [gt_labels,pred_labels,testAccuracy] = evaluate_model(testDataUpper, model_upper, params_upper);
 
-[~, prec, recall, fscore] = prec_recall(gt_labels,pred_labels);
+% [confMAT, prec, recall, fscore] = prec_recall(YYYY,YYY);
 
-
+[confMAT, prec, recall, fscore] = prec_recall(gt_labels,pred_labels);
 end
